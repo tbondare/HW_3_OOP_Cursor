04dc2dfe34600c8d8ce08b21973dda716ce94e3b
tbondare <tanya.bon2016@gmail.com>
2021-04-02 23:45:37 +0300

    did first ex

---
+++ shape hierarchy/src/main/java/Vertex.java
@@ -0,0 +1,20 @@
+public class Vertices {
+    private int x;
+    private int y;
+    private int z;
+
+    public Vertices() {}
+
+    public Vertices(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Vertices(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+
+}
